de777240ade63f2363f91a60d9477f2ce645e527
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-06-10 14:54:54 +0200

    initial set up

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS08-main/src/main.jsx
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS08-main/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
+function App() {
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+  <Routes>
+     <Route path="/" element={<App />} />
+  </Routes>
+  </BrowserRouter>
+);
