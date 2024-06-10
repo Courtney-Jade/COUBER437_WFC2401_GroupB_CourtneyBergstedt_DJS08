1aa66af705a26fb9edc343580589ec31b74b31a5
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-06-10 15:00:57 +0200

    changed code for van detail

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS08-main/Host/HostVanDetail.jsx
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS08-main/Host/HostVanDetail.jsx
@@ -1,15 +1,88 @@
 import React from "react"
-import { useOutletContext } from "react-router-dom"
+import { useParams, Link, NavLink, Outlet } from "react-router-dom"
+import { getHostVans } from "../../api"
 
-export default function HostVanInfo() {
-    const { currentVan } = useOutletContext()
-    
+export default function HostVanDetail() {
+    const [currentVan, setCurrentVan] = React.useState(null)
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
+    const { id } = useParams()
+
+    React.useEffect(() => {
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getHostVans(id)
+                setCurrentVan(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadVans()
+    }, [id])
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return (
-        <section className="host-van-detail-info">
-            <h4>Name: <span>{currentVan.name}</span></h4>
-            <h4>Category: <span>{currentVan.type}</span></h4>
-            <h4>Description: <span>{currentVan.description}</span></h4>
-            <h4>Visibility: <span>Public</span></h4>
+        <section>
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+            {currentVan &&
+                <div className="host-van-detail-layout-container">
+                    <div className="host-van-detail">
+                        <img src={currentVan.imageUrl} />
+                        <div className="host-van-detail-info-text">
+                            <i
+                                className={`van-type van-type-${currentVan.type}`}
+                            >
+                                {currentVan.type}
+                            </i>
+                            <h3>{currentVan.name}</h3>
+                            <h4>${currentVan.price}/day</h4>
+                        </div>
+                    </div>
+
+                    <nav className="host-van-detail-nav">
+                        <NavLink
+                            to="."
+                            end
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                        >
+                            Details
+                    </NavLink>
+                        <NavLink
+                            to="pricing"
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                        >
+                            Pricing
+                    </NavLink>
+                        <NavLink
+                            to="photos"
+                            style={({ isActive }) => isActive ? activeStyles : null}
+                        >
+                            Photos
+                    </NavLink>
+                    </nav>
+                    <Outlet context={{ currentVan }} />
+                </div>}
         </section>
     )
 }
